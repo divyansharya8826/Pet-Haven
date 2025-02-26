4d70f7fa2080f5bc8b182df95c0f41a961968347
VK9175 <160104958+VK9175@users.noreply.github.com>
2025-02-26 14:16:58 +0530

    Added modal for dog details

--- routes.py
+++ routes.py
@@ -17,7 +17,6 @@ def dog_details(dog_id):
     dog = Dogs.query.get(dog_id)
     if not dog:
         return jsonify({"error": "Dog not found"}), 404
-    return render_template('dog_details.html', dog=dog)
 
 
 @app.route('/api/dogs', methods=['GET'])
